6a0fb5c7045eff9278842451974f1d5265e0070e
DeveloperTunde <85291994+DeveloperTunde@users.noreply.github.com>
2025-03-20 13:49:44 +0000

    update: updated files and folders

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,91 +1,91 @@
-const jwt = require('jsonwebtoken');
-const { callEthContract, callPolygonContract, callBscContract, callArbitrumContract, callAvalancheContract, callFantomContract, callHarmonyContract, callHecoContract, callKlayContract, callMaticContract, callMoonbeamContract, callHashedContract, callOptimismContract, callPalmContract, callRoninContract, callXDaiContract } = require('../config/getContract')
+// const jwt = require('jsonwebtoken');
+// const { callEthContract, callPolygonContract, callBscContract, callArbitrumContract, callAvalancheContract, callFantomContract, callHarmonyContract, callHecoContract, callKlayContract, callMaticContract, callMoonbeamContract, callHashedContract, callOptimismContract, callPalmContract, callRoninContract, callXDaiContract } = require('../config/getContract')
 
-const EthContact = (() => {
-  callEthContract();
-});
+// const EthContact = (() => {
+//   callEthContract();
+// });
 
-const PolygonContact = (() => {
-  callPolygonContract();
-});
+// const PolygonContact = (() => {
+//   callPolygonContract();
+// });
 
-const BscContact = (() => {
-  callBscContract();
-});
+// const BscContact = (() => {
+//   callBscContract();
+// });
 
-const ArbitrumContact = (() => {
-  callArbitrumContract();
-});
+// const ArbitrumContact = (() => {
+//   callArbitrumContract();
+// });
 
-const AvalancheContact = (() => {
-  callAvalancheContract();
-});
+// const AvalancheContact = (() => {
+//   callAvalancheContract();
+// });
 
-const FantomContact = (() => {
-  callFantomContract();
-});
+// const FantomContact = (() => {
+//   callFantomContract();
+// });
 
-const HarmonyContact = (() => {
-  callHarmonyContract();
-});
+// const HarmonyContact = (() => {
+//   callHarmonyContract();
+// });
 
-const HecoContact = (() => {
-  callHecoContract();
-});
+// const HecoContact = (() => {
+//   callHecoContract();
+// });
 
-const KlayContact = (() => {
-  callKlayContract();
-});
+// const KlayContact = (() => {
+//   callKlayContract();
+// });
 
-const MaticContact = (() => {
-  callMaticContract();
-});
+// const MaticContact = (() => {
+//   callMaticContract();
+// });
 
-const MoonbeamContact = (() => {
-  callMoonbeamContract();
-});
+// const MoonbeamContact = (() => {
+//   callMoonbeamContract();
+// });
 
-const HashedContact = (() => {
-  callHashedContract();
-})();
+// const HashedContact = (() => {
+//   callHashedContract();
+// })();
 
-const OptimismContact = (() => {
-  callOptimismContract();
-});
+// const OptimismContact = (() => {
+//   callOptimismContract();
+// });
 
-const PalmContact = (() => {
-  callPalmContract();
-});
+// const PalmContact = (() => {
+//   callPalmContract();
+// });
 
-const RoninContact = (() => {
-  callRoninContract();
-});
+// const RoninContact = (() => {
+//   callRoninContract();
+// });
 
-const XDaiContact = (() => {
-  callXDaiContract();
-});
+// const XDaiContact = (() => {
+//   callXDaiContract();
+// });
 
-module.exports = function (req, res, next) {
-  // Get token from header
-  const token = req.header('x-auth-token');
+// module.exports = function (req, res, next) {
+//   // Get token from header
+//   const token = req.header('x-auth-token');
 
-  // Check if not token
-  if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
-  }
+//   // Check if not token
+//   if (!token) {
+//     return res.status(401).json({ msg: 'No token, authorization denied' });
+//   }
 
-  // Verify token
-  try {
-    jwt.verify(token, "hello", (error, decoded) => {
-      if (error) {
-        return res.status(401).json({ msg: 'Token is not valid' });
-      } else {
-        req.user = decoded.user;
-        next();
-      }
-    });
-  } catch (err) {
-    console.error('something wrong with auth middleware');
-    res.status(500).json({ msg: 'Server Error' });
-  }
-};
+//   // Verify token
+//   try {
+//     jwt.verify(token, "hello", (error, decoded) => {
+//       if (error) {
+//         return res.status(401).json({ msg: 'Token is not valid' });
+//       } else {
+//         req.user = decoded.user;
+//         next();
+//       }
+//     });
+//   } catch (err) {
+//     console.error('something wrong with auth middleware');
+//     res.status(500).json({ msg: 'Server Error' });
+//   }
+// };
